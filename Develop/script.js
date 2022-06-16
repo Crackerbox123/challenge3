6b5a65a337b257b7730a918480e0f5b6d78099b8
Kal O <koestreicher4@gmail.com>
2022-06-15 21:08:13 -0500

    reheirachied JS functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,130 +1,108 @@
 // Assignment code here
-
-var passwordLength;
-var passwordLower;
-var passwordUpper;
-var passwordNumeric;
-var passwordSpecial;
-var attributesGood;
-
-
-// Class to hold password attributes
-class passwordAtt{
-  constructor(length, lowers, uppers, numerics, specials){
-    this.length = length;
-    this.lowers = lowers;
-    this.uppers = uppers;
-    this.numerics = numerics;
-    this.specials = specials;
+// random variables
+const lCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+const uCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const speclChar = ["/", "*", "-", "+", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "?", ";", ":", ".", "<", ">", "[", "]", "{", "}", "|"];
+var allChar = [];
+var guaranteedChars = [];
+var passLength = 0;
+function getRandom(arr) {
+  // generate a random number based on the length of the array parameter
+  // grab an element based on the randomly generated number ref 65
+  // return the randomly generated element
+  guaranteedChars.push(arr[Math.floor(Math.random() * arr.length)]);
+};
+// Getting Password Parameters
+var getPassPerameters = function () {
+  passLength = window.prompt("How many characters would you like your password to be? Between 8 and 128.")
+  while (passLength < 8 || passLength > 128) {
+    window.alert("Please, enter a number between 8 and 128.")
+    passLength = prompt("How many characters would you like your password to be? Between 8 and 128.")
   }
-}
+  var lowerCase =
+    window.confirm("Would you like your password to include lowercase letters?");
 
-// Function for gathering password critera with windows and constructing the passwordAtt
-function gatherCritera() {
+  var upperCase =
+    window.confirm("Would you like your password to include UPPERCASE LETTERS?");
 
-    // Gather password critera (length = 8-128 chars)
-  const length = Number(window.prompt("Please enter length of password between 8-128 characters:", ""));
-  var goodLength;
-  if(length >= 8 && length <= 128){
-    passwordLength = length;
-    // console.log("password length : " + passwordLength);
-    goodLength = true;
+  var numeric =
+    window.confirm("Would you like your password to include Numbers?");
+
+  var specialChars =
+    window.confirm("Would you like your password to include Special Characters?");
+  // Generated password info object
+  var generatedPasswordInfo = {
+    passwordLength: parseInt(passLength),
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+    numeric: numeric,
+    specialChars: specialChars
+  };
+  console.log("Set password length is " + passLength + " Characters")
+  console.log(generatedPasswordInfo)
+  // make sure at least one option is picked
+  if (generatedPasswordInfo.upperCase === false && generatedPasswordInfo.lowerCase === false && generatedPasswordInfo.numeric === false && generatedPasswordInfo.specialChars === false) {
+    window.alert("Please select at least one option")
+    getPassPerameters();
   }
+  // generate array for password generation
+  if (generatedPasswordInfo.lowerCase) {
+    allChar = allChar.concat(lCase);
+    // add getRandom()
+    getRandom(lCase);
+    // add result of getRandom to guaranteedChars
+  };
+  if (generatedPasswordInfo.upperCase) {
+    allChar = allChar.concat(uCase);
+    getRandom(uCase);
+  };
+  if (generatedPasswordInfo.numeric) {
+    allChar = allChar.concat(numbers);
+    getRandom(numbers);
+  };
+  if (generatedPasswordInfo.specialChars) {
+    allChar = allChar.concat(speclChar);
+    getRandom(speclChar)
+  };
+  // generate password
+  var generatedPasswordArr = [];
+  // Generate random password
+  var generatePassword = function () {
+    for (var i = 0; i <= generatedPasswordInfo.passwordLength - 1; i++) {
 
-  // Gather password critera (lower, upper, numeric, special characters)
-  var lower = window.confirm("Include lower case characters in password?");
-  passwordLower = lower;
-  // console.log(passwordLower);
-
-  var upper = window.confirm("Include upper case characters in password?");
-  passwordUpper = upper;
-  // console.log(passwordUpper);
-
-  var numeric = window.confirm("Include numeric characters in password?");
-  passwordNumeric = numeric;
-  // console.log(passwordNumeric);
-
-  var special = window.confirm("Incluse special characters in password?");
-  passwordSpecial = special;
-  // console.log(passwordSpecial);
-
-  if(goodLength){
-    if( lower || upper || numeric || special){
-      attributesGood = true;
-      // console.log("The attributes are passable. attributesGood = " + attributesGood);
-    }
-    else {
-      attributesGood = false;
-      // console.log("The attributes are NOT passable. A character type was not selected. attributesGood = " + attributesGood);  
-    }
+      // console.log(Math.floor(Math.random() * allChar.length));
+      generatedPasswordArr.push(allChar[Math.floor(Math.random() * allChar.length)]);
+    };
+    // splice the generatedPasswordArr
+    generatedPasswordArr.splice(
+      // Start splicing at the start of the generatedPasswordArr
+      0,
+      // Remove the lenght of the guaranteedChars from the generatedPasswordArr
+      guaranteedChars.length,
+      // insert the guaranteedChars Array
+      guaranteedChars.join(""))
+  }
+  generatePassword();
+  console.log("this is the guarenteedChars array " + guaranteedChars)
+  console.log("this is the generated password array " + generatedPasswordArr)
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatedPasswordArr.join("");
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
     
   }
-  else 
-  { attributesGood = false;
-    // console.log("The attributes are NOT passable. Password Length too short. attributesGood = " + attributesGood);  
-  }
-
-}
-
-
+  writePassword();
+  guaranteedChars = [];
+  generatedPasswordArr = [];
+  allChar = [];
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-function generatePassword() {
-  const lowerAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const upperAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "-", "<", ">", "/", "?"];
-  let passwordArray = [];
-
-  gatherCritera();
-  const myPassword = new passwordAtt(passwordLength, passwordLower, passwordUpper, passwordNumeric, passwordSpecial);
-  // console.log(myPassword) //verify values were properly entered into passwordAtt class
-  // console.log(myPassword.passwordLower)
-  if(!attributesGood) {
-    window.confirm("You must enter a password length of 8-128 and select at least one character type.")
-    return "Please Try Again"
-  }
-  else {
-    if(myPassword.lowers == true){
-      passwordArray.push(...lowerAlphabet);
-    }
-    if(myPassword.uppers == true){
-      passwordArray.push(...upperAlphabet);
-    }
-    if(myPassword.numerics == true){
-      passwordArray.push(...numerals);
-    }
-    if(myPassword.specials == true){
-      passwordArray.push(...specialChars);
-    }
-
-    // console.log(passwordArray);
-
-    // console.log(myPassword.length);
-
-    let finalPass = [];
-    for( let i = 0; i < myPassword.length; i++){
-      var j = Math.floor(Math.random() * (passwordArray.length - 1));
-      finalPass[i] = passwordArray[j];
-      // console.log(i);
-    }
-
-    // console.log(finalPass);
-
-    return finalPass.join("");
-
-  }
-}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPassPerameters);
+
